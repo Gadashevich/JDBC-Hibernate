0accf4c28ea04dc8e062fac3b09eae5b4b2d45c0
GadashJohn <fox5b21@gmail.com>
2023-01-31 20:37:27 +0300

    Hibernate end

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,5 @@
+import dao.UserDao;
+import dao.UserDaoHibernateImpl;
 import dao.UserDaoJDBCImpl;
 import model.User;
 
@@ -6,8 +8,12 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-        UserDaoJDBCImpl userDao = new UserDaoJDBCImpl();
-        userDao.dropUsersTable();
+        scriptHibernate();
+    }
+
+    private static void scriptJDBC() {
+        UserDao userDao = new UserDaoJDBCImpl();
+        // userDao.dropUsersTable();
         userDao.createUsersTable();
         userDao.saveUser("IVAN","lastName",(byte)20);
         userDao.saveUser("PETR","PETROV",(byte)17);
@@ -19,6 +25,29 @@ public class Main {
         for (User user : allUsers) {
             System.out.println(user);
         }
+        userDao.cleanUsersTable();
+        userDao.dropUsersTable();
     }
+
+    private static void scriptHibernate() {
+        UserDao userDao = new UserDaoHibernateImpl();
+        // userDao.dropUsersTable();
+        userDao.createUsersTable();
+        userDao.saveUser("IVAN","lastName",(byte)20);
+        userDao.saveUser("PETR","PETROV",(byte)17);
+        userDao.saveUser("DIMA","DIMA",(byte)23);
+        userDao.saveUser("SEREJA","Serg",(byte)21);
+        userDao.saveUser("Shrek","lolo",(byte)36);
+        userDao.saveUser("ARTUR","King",(byte)57);
+        List<User> allUsers = userDao.getAllUsers();
+        for (User user : allUsers) {
+            System.out.println(user);
+        }
+        userDao.cleanUsersTable();
+        userDao.dropUsersTable();
+    }
+
+
+
 
 }
